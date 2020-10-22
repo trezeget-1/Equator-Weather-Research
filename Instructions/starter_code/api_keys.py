288b68292ffbe2373a73b6da73c68f333c13e57d
trezeget-1 <71102019+trezeget-1@users.noreply.github.com>
2020-10-22 17:24:24 -0500

    Made some initial progress

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2c8e719b1a20723fca575c2549275a18"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDX1Pc65pFo5viVcI3wBctQnkHCdpb27Gw"
